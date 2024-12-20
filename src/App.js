c779bf43dbe948a3c56fac65e12eec6542e08369
Rayyan-Karimi <rayyan.karimi.30.3@mountblue.tech>
2024-12-20 21:34:33 +0530

    Changes to add header

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import "./App.css";
 import Board from "./Board.js";
 import React from "react";
 
+function Hero() {
+  return (
+    <div className="py-6 text-center shadow-lg w-full text-xl font-semibold">
+      <h2>Grid Lights</h2>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App flex justify-center items-center min-w-screen min-h-screen">
+      <Hero />
       <Board />
     </div>
   );
